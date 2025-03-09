f018d9bde6ff0639c1bbcf2c79d217349ea66873
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-03-09 12:14:44 +0100

    disabled oauth2, working login

--- backend/central-persistence-service/src/main/java/com/blck/central_persistence_service/security/AuthController.java
+++ backend/central-persistence-service/src/main/java/com/blck/central_persistence_service/security/AuthController.java
@@ -9,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,12 +28,12 @@ import reactor.core.publisher.Mono;
 public class AuthController {
 
 	private final ReactiveAuthenticationManager reactiveAuthenticationManager;
-	private final HttpSessionSecurityContextRepository securityContextRepository;
+	private final WebSessionServerSecurityContextRepository securityContextRepository;
 	private final AccountService accountService;
 
 	@Autowired
 	public AuthController(ReactiveAuthenticationManager reactiveAuthenticationManager,
-						  HttpSessionSecurityContextRepository securityContextRepository,
+						  WebSessionServerSecurityContextRepository securityContextRepository,
 						  AccountService accountService) {
 		this.reactiveAuthenticationManager = reactiveAuthenticationManager;
 		this.securityContextRepository = securityContextRepository;
@@ -36,7 +41,7 @@ public class AuthController {
 	}
 
 	@PostMapping("/register")
-	public Mono<ResponseEntity<UserAccount>> register(@RequestBody JsonNode credentials, HttpServletRequest request, HttpServletResponse response) {
+	public Mono<ResponseEntity<UserAccount>> register(@RequestBody JsonNode credentials) {
 		String username = credentials.get("username").asText();
 		String password = credentials.get("password").asText();
 		return accountService.registerUser(username, password)
@@ -45,19 +50,20 @@ public class AuthController {
 	}
 
 	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Mono<ResponseEntity<String>> login(@RequestBody JsonNode credentials, HttpServletRequest request, HttpServletResponse response) {
+	public Mono<ResponseEntity<String>> login(@RequestBody JsonNode credentials, ServerWebExchange exchange) {
 		Authentication authRequest = new UsernamePasswordAuthenticationToken(
 				credentials.get("username").asText(),
 				credentials.get("password").asText()
 		);
+
 		return reactiveAuthenticationManager.authenticate(authRequest)
-				.doOnNext(authResponse -> {
-					SecurityContext securityContext = SecurityContextHolder.getContext();
-					securityContext.setAuthentication(authResponse);
-					securityContextRepository.saveContext(securityContext, request, response);
+				.flatMap(authResponse -> {
+					SecurityContext securityContext = new SecurityContextImpl(authResponse);
+					return securityContextRepository.save(exchange, securityContext)
+							.then(Mono.just(ResponseEntity.ok("Authentication Successful")));
 				})
-				.map(authResponse -> ResponseEntity.ok("Authentication Successful"))
 				.onErrorResume(e -> Mono.just(new ResponseEntity<>("Authentication failure", HttpStatus.UNAUTHORIZED)));
 	}
+
 
 }
--- backend/central-persistence-service/src/main/java/com/blck/central_persistence_service/security/SecurityConfiguration.java
+++ backend/central-persistence-service/src/main/java/com/blck/central_persistence_service/security/SecurityConfiguration.java
@@ -1,65 +1,63 @@
 package com.blck.central_persistence_service.security;
 
 import com.blck.central_persistence_service.accounts.AccountService;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import com.nimbusds.jose.jwk.RSAKey;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.UUID;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
+//	@Order(2)
+	@Bean
+	public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http) {
+		http
+				.csrf().disable()
+				.authorizeExchange(exchanges -> exchanges
+						.anyExchange().permitAll()
+				);
+		return http.build();
+	}
+
+	@Bean
+	public HttpSessionSecurityContextRepository getSecurityContextRepository() {
+		return new HttpSessionSecurityContextRepository();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ReactiveUserDetailsService userDetailsService(AccountService accountService) {
+		return accountService;
+	}
+
+	@Bean
+	public WebSessionServerSecurityContextRepository securityContextRepository() {
+		return new WebSessionServerSecurityContextRepository();
+	}
+
+	@Bean
+	public ReactiveAuthenticationManager reactiveAuthenticationManager(ReactiveUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		UserDetailsRepositoryReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+		authenticationManager.setPasswordEncoder(passwordEncoder);
+		return authenticationManager;
+	}
+
+// TODO: OIDC below
 //	@Bean
 //	@Order(1)
 //	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -85,113 +83,56 @@ public class SecurityConfiguration {
 //
 //		return http.build();
 //	}
-
-//	@Order(2)
-	@Bean
-	public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http) {
-		http
-				.csrf().disable();
-//				.sessionManagement(session -> session
-//						.sessionCreationPolicy(SessionCreationPolicy.NEVER)
-//						.maximumSessions(1)
-//				)
-//				.securityContext((securityContext) -> securityContext
-//						.requireExplicitSave(true)
-//						.securityContextRepository(getSecurityContextRepository())
-//				);
-		// TODO: fix authorize rules vs level-preauthorize
-
-//				.authorizeHttpRequests((authorize) -> authorize
-//						.requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
-//						.requestMatchers(HttpMethod.GET, "/users/**").hasAnyRole("USER", "ADMIN")
-//						.anyRequest().authenticated()
-//				);
-		return http.build();
-	}
-
-	@Bean
-	public HttpSessionSecurityContextRepository getSecurityContextRepository() {
-		return new HttpSessionSecurityContextRepository();
-	}
-
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public ReactiveUserDetailsService userDetailsService(AccountService accountService) {
-		return accountService::findByUsername;
-	}
-
-	@Bean
-	public ReactiveAuthenticationManager reactiveAuthenticationManager(ReactiveUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-		UserDetailsRepositoryReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
-		authenticationManager.setPasswordEncoder(passwordEncoder);
-		return authenticationManager;
-	}
-
-	@Bean
-	public ReactiveClientRegistrationRepository reactiveClientRegistrationRepository() {
-		ClientRegistration oidcClient = ClientRegistration.withRegistrationId("oidc-client")
-				.clientId("oidc-client")
-				.clientSecret("{noop}secret")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-				.scope(OidcScopes.OPENID)
-				.scope(OidcScopes.PROFILE)
-				.build();
-		return new InMemoryReactiveClientRegistrationRepository(oidcClient);
-	}
-
-	@Bean
-	public JWKSource<SecurityContext> jwkSource() {
-		KeyPair keyPair = generateRsaKey();
-		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-		RSAKey rsaKey = new RSAKey.Builder(publicKey)
-				.privateKey(privateKey)
-				.keyID(UUID.randomUUID().toString())
-				.build();
-		JWKSet jwkSet = new JWKSet(rsaKey);
-		return new ImmutableJWKSet<>(jwkSet);
-	}
-
-	private static KeyPair generateRsaKey() {
-		KeyPair keyPair;
-		try {
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-			keyPairGenerator.initialize(2048);
-			keyPair = keyPairGenerator.generateKeyPair();
-		}
-		catch (Exception ex) {
-			throw new IllegalStateException(ex);
-		}
-		return keyPair;
-	}
-
-	@Bean
-	public ReactiveJwtDecoder jwtDecoder() {
-		return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8080");
-	}
-
-	@Bean
-	public AuthorizationServerSettings authorizationServerSettings() {
-		return AuthorizationServerSettings.builder().build();
-	}
-
-	//	@Bean
-//	public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
 //
-//		UserDetails userDetails = User.builder()
-//				.username("user")
-//				.password(passwordEncoder.encode("password"))
-//				.roles(String.valueOf(Roles.USER))
+//	@Bean
+//	public ReactiveClientRegistrationRepository reactiveClientRegistrationRepository() {
+//		ClientRegistration oidcClient = ClientRegistration.withRegistrationId("oidc-client")
+//				.clientId("oidc-client")
+//				.clientSecret("{noop}secret")
+//				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+//				.scope(OidcScopes.OPENID)
+//				.scope(OidcScopes.PROFILE)
 //				.build();
-//		return new InMemoryUserDetailsManager(userDetails);
+//		return new InMemoryReactiveClientRegistrationRepository(oidcClient);
+//	}
+//
+//	@Bean
+//	public JWKSource<SecurityContext> jwkSource() {
+//		KeyPair keyPair = generateRsaKey();
+//		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+//		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+//		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+//				.privateKey(privateKey)
+//				.keyID(UUID.randomUUID().toString())
+//				.build();
+//		JWKSet jwkSet = new JWKSet(rsaKey);
+//		return new ImmutableJWKSet<>(jwkSet);
+//	}
+//
+//	private static KeyPair generateRsaKey() {
+//		KeyPair keyPair;
+//		try {
+//			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+//			keyPairGenerator.initialize(2048);
+//			keyPair = keyPairGenerator.generateKeyPair();
+//		}
+//		catch (Exception ex) {
+//			throw new IllegalStateException(ex);
+//		}
+//		return keyPair;
+//	}
+//
+//	@Bean
+//	public ReactiveJwtDecoder jwtDecoder() {
+//		return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8080/.well-known/openid-configuration");
+//	}
+//
+//	@Bean
+//	public AuthorizationServerSettings authorizationServerSettings() {
+//		return AuthorizationServerSettings.builder().build();
 //	}
 
 }
