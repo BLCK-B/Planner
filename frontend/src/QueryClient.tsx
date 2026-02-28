3cb2187c5009b673983a9680b224b5faca371141
BLCK <123077751+BLCK-B@users.noreply.github.com>
2026-02-28 14:55:31 +0100

    test auth uri

--- frontend/src/QueryClient.tsx
+++ frontend/src/QueryClient.tsx
@@ -1,8 +1,7 @@
 import {QueryClient, QueryCache, MutationCache} from '@tanstack/react-query';
 import type {FetchError} from '@/types/FetchError.ts';
 
-// todo: server - redirect to backend endpoint or just auth
-const URL = (import.meta.env.VITE_API_URL as string | undefined) ? "https://auth.spruits.eu" : "http://localhost:8081/oauth2/authorization/zitadel";
+const URL = (import.meta.env.VITE_API_URL as string | undefined) ? "https://test-planner-api.spruits.eu/login/oauth2/code/zitadel" : "http://localhost:8081/oauth2/authorization/zitadel";
 
 export const queryClient = new QueryClient({
     defaultOptions: {
