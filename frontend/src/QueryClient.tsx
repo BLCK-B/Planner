df77752fb38d586657559d8dd7429d3a58fdb84a
BLCK <123077751+BLCK-B@users.noreply.github.com>
2026-02-28 16:54:31 +0100

    redirect url

--- frontend/src/QueryClient.tsx
+++ frontend/src/QueryClient.tsx
@@ -1,7 +1,7 @@
 import {QueryClient, QueryCache, MutationCache} from '@tanstack/react-query';
 import type {FetchError} from '@/types/FetchError.ts';
 
-const URL = (import.meta.env.VITE_API_URL as string | undefined) ? "https://test-planner-api.spruits.eu/login/oauth2/code/zitadel" : "http://localhost:8081/oauth2/authorization/zitadel";
+const URL = (import.meta.env.VITE_API_URL as string | undefined) ? "https://test-planner-api.spruits.eu/oauth2/code/zitadel" : "http://localhost:8081/oauth2/authorization/zitadel";
 
 export const queryClient = new QueryClient({
     defaultOptions: {
